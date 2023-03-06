e54ed5e5dbec8115ff115a223b1e31fbf7fe3f59
sasha <alex-p14@bk.ru>
2023-03-06 19:03:09 +0300

    refactoring src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,9 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcomeUser ()
+function welcomeUser()
 {
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
-
