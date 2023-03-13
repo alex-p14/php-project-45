b3f881c2d7946634c60987c7cbacd84169be5368
sasha <alex-p14@bk.ru>
2023-03-13 16:12:33 +0300

    Add src/Games/even.php

---
+++ src/Games/even.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Brain\Games\Games\Even;
+
+function getDescription()
+{
+    return fn() => 'Answer "yes" if the number is even, otherwise answer "no".';
+}
+
+function getQuestionAnswer()
+{
+    return function () {
+        $question = mt_rand(1, 100);
+        $answer = $question % 2 === 0 ? 'yes' : 'no';
+        return [$question, $answer];
+    };
+}
