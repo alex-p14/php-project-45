1c830e19418d98a68855c0317d9fbdefb0b23b7e
sasha <alex-p14@bk.ru>
2023-03-13 16:11:09 +0300

    Refactoring src/Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,23 +5,17 @@ namespace Brain\Games\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function welcomeUser()
+function runGame($description, $getQuestionAnswer)
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return $name;
-}
-
-function runGame($name)
-{
-    line('Answer "yes" if the number is even, otherwise answer "no".');
+    line($description());
     $amountRounds = 3;
     for ($i = 0; $i < $amountRounds; $i++) {
-        $question = mt_rand(1, 100);
+        [$question, $rAns] = $getQuestionAnswer();
         line("Quwstion: %s", $question);
         $ans = prompt('Your answer');
-        $rAns = $question % 2 === 0 ? 'yes' : 'no';
         if ($ans != $rAns) {
             line("'%s' is wrong answer ;(. Correct answer was '%s'.", $ans, $rAns);
             line("Let's try again, %s!", $name);
