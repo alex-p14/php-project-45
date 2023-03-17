d7e7c9d32b87e731f675486e2a1b41d0b7b00775
sasha <alex-p14@bk.ru>
2023-03-17 11:36:38 +0300

    Add src/Games/Prime.php

---
+++ src/Games/Prime.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Brain\Games\Games\Prime;
+
+function getDescription()
+{
+    return function () {
+        return 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    };
+}
+
+function isPrime($num)
+{
+    $halfNum = intdiv($num, 2);
+    for ($i = 2; $i <= $halfNum; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return $num === 1 ? false : true;
+}
+
+function getQuestionAnswer()
+{
+    return function () {
+        $question = mt_rand(1, 100);
+        $answer = isPrime($question) ? 'yes' : 'no';
+        return [$question, $answer];
+    };
+}
