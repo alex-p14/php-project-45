bf3113545469147c23e57f62d9bf82dcc3d6dc02
alex <alex-p14@bk.ru>
2023-04-07 16:51:41 +0300

    Refactoring file src/Engine.php
    Improve code readability.

--- src/Engine.php
+++ src/Engine.php
@@ -12,18 +12,20 @@ function runGame(string $description, callable $getQuestionAnswer)
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($description);
+
     $amountRounds = 3;
     for ($i = 0; $i < $amountRounds; $i++) {
         [$question, $rightAnswer] = $getQuestionAnswer();
         line("Question: %s", $question);
         $answer = prompt('Your answer');
+
         if ($answer != $rightAnswer) {
-            out("'%s' is wrong answer ;(. ", $answer);
-            line("Correct answer was '%s'.", $rightAnswer);
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $rightAnswer);
             line("Let's try again, %s!", $name);
             return;
         }
         line('Correct!');
     }
+
     line("Congratulations, %s!", $name);
 }
